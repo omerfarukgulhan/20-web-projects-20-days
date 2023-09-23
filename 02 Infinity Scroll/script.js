a706bc16b25fd41cab7f5b59020ab6fa7de357f9
omerfarukgulhan <omerfarukgulhan@outlook.com>
2023-09-23 18:35:03 +0300

    infinity scroll finished

--- 02 Infinity Scroll/script.js
+++ 02 Infinity Scroll/script.js
@@ -1,1 +1,81 @@
-console.log("works")
+// Api key imported from other keys.js file
+// Unsplash api
+const count = 10;
+const apiKey = unsplashApiKey;
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Html elements
+const imageContainer = document.querySelector(".image-container");
+const loader = document.querySelector(".loader");
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+getPhotos();
+
+window.addEventListener("scroll", endOfPage);
+
+// Get photos from api
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    alert(error)
+  }
+}
+
+// Create html elements
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+
+    const img = document.createElement("img");
+
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    img.addEventListener("load", imageLoaded);
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+// Function for setting attirbutes
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  console.log(imagesLoaded);
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
+// Load more photos at bottom
+function endOfPage() {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+}
