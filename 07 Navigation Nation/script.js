150b1a3bd01cf28632299532e972285936830f6c
omerfarukgulhan <omerfarukgulhan@outlook.com>
2023-09-28 13:40:57 +0300

    nav items simplified with classes

--- 07 Navigation Nation/script.js
+++ 07 Navigation Nation/script.js
@@ -1,12 +1,6 @@
-const menuBars = document.getElementById("menu-bars");
-const overlay = document.getElementById("overlay");
-const nav1 = document.getElementById("nav-1");
-const nav2 = document.getElementById("nav-2");
-const nav3 = document.getElementById("nav-3");
-const nav4 = document.getElementById("nav-4");
-const nav5 = document.getElementById("nav-5");
-
-const navItems = [nav1, nav2, nav3, nav4, nav5];
+const menuBars = document.querySelector("#menu-bars");
+const overlay = document.querySelector("#overlay");
+const navItems = document.querySelectorAll(".nav-item");
 
 // Event Listeners
 eventListeners();
